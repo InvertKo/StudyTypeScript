d9164ff53147cad265b4328f0d897b8f9ce7ac5c
invertko <jonghyun4501@naver.com>
2020-05-25 21:59:21 +0900

    '20200525'

---
+++ asyncBykim.ts
@@ -0,0 +1,26 @@
+const puppeteer1 = require("puppeteer");
+
+async function getFile() {
+    const browser = await puppeteer1.launch(
+        { executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe' },
+        { headless: true }
+    );
+    const page = await browser.newPage();
+    await page.goto("https://finance.daum.net/domestic/kospi200");
+
+    const result = await page.evaluate(()=>{
+        const lastCommitMessage = (document.querySelector(
+            "#boxEntryChange > div.box_contents > div > table > tbody > tr > th > a"
+            ) as HTMLSpanElement)?.innerText
+
+            const readme = document.querySelector(
+                "#boxEntryChange > div.box_contents > div > table > thead > tr > th:nth-child(1)"
+            );
+        return{lastCommitMessage,readme}    
+    })//result
+
+    console.log(result)
+    await browser.close()
+}//getFile()
+
+getFile().catch(err => console.error(err))
---
+++ promiseByKim.ts
@@ -0,0 +1,36 @@
+const puppeteer2 = require("puppeteer");
+
+function getFile1 () {
+    return puppeteer2.launch().then(browser => {
+      return browser.newPage().then(page => {
+        // 나중을 위해 browser도 같이 반환해줘야함.
+        return { page, browser }
+      })
+    }).then(({ page, browser }) => {
+      return page.goto(
+        "https://github.com/puppeteer/puppeteer/blob/master/README.md"
+      ).then(() => {
+        return page.evaluate(() => {
+          // 마지막 커밋 메세지를 가져옴
+          // document.querySelector("span.lh-default.v-align-middle")가 undefined면 .innerText도 undefined임.
+          const lastCommitMessage = (document.querySelector(
+            "span.lh-default.v-align-middle"
+          ) as HTMLSpanElement)?.innerText
+  
+          // Markdown body의 내용 html을 가져옴
+          // document.querySelector(".markdown-body.entry-content")가 undefined면 .innerHTML도 undefined임.
+          const readme = document.querySelector(".markdown-body.entry-content")
+            ?.innerHTML
+  
+          return { lastCommitMessage, readme }
+        })
+      }).then(result => {
+        return { result, browser }
+      })
+    }).then(({ result, browser }) => {
+      console.log(result)
+      return browser.close()
+    })
+  }
+  
+  getFile1().catch(err => console.error(err))
