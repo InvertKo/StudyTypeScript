20ec87b47163c3d1a851352777875e2bab2a52c9
invertko <jonghyun4501@naver.com>
2020-05-28 18:46:23 +0900

    '20200528_vuejs_start'

---
+++ start-vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span>{{message}}</span><br/>
+    <span v-html="message"></span>
+  </div>
+</template>
+
+<script lang='js'>
+export default{
+  name:'Hello Vue js',
+  data:()=>{
+    return{
+      message:'안녕.<span style="color:red">Vue</span>'
+    };
+  },
+};
+</script>
